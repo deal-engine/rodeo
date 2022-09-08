e827e50295e04af57d345fd92cc41015795a7d1a
Victor Hugo Borja <vborja@apache.org>
2022-09-08 13:26:44 -0500

    Adding two chapters

---
+++ rodeo/src/rodeo/00-intro.scala
@@ -0,0 +1,19 @@
+package rodeo
+
+/**
+ * Welcome to DealEngine scala rodeo.
+ *
+ * This material is intended for you to learn the basic of scala and some
+ * common functional idioms.
+ *
+ * The first level will cover:
+ *
+ * building of
+ *   Variables
+ *   Tuples
+ *   List
+ */
+object Intro {
+
+
+}
