3654a9ecd40eee881524e8e20226bd627e5c12de
Victor Hugo Borja <vborja@apache.org>
2022-09-08 14:43:10 -0500

    Introduction to Option
    Co-authored-by: Fabián Heredia Montiel <fabianhjr@protonmail.com>

---
+++ rodeo/src/rodeo/02-option.scala
@@ -0,0 +1,39 @@
+package rodeo
+
+object Options {
+
+  // Options are used to represent the presence of some value.
+  // Some other non-functional languages use NULL to indicate absence.
+  //
+  // However this can lead to unchecked errors, since people cannot know
+  // if the value exists unless they explictly check with `!= null`
+  //
+  // For this reason, in Scala it's a common idiom to use the Option type instead.
+  // For example: the type Option[String] expresses the idea of an String that may
+  // exist or not.
+
+  // Excercise: Using the Some(_) constructor, change the value assigned to
+  // this variable to be some integer 42.
+  val theAnwserToTheUniverseAndEverything: Option[Int] = None
+
+  // Option is one of the simplest pure functional data structures
+  //
+  // It is a great opportunity to take a look at pattern matching!
+  //
+  // Pattern matching allows us to "switch" depending on the
+  // structure of the value it is passed.
+  // Excercise: Change this code so that if theAnswerToTheUniverseAndEverything
+  // holds some value, the `youKnow` variable gets assigned to "you know it all".
+  val youKnow: String = theAnwserToTheUniverseAndEverything match {
+    case None         => "you know nothing"
+    case Some(answer) => ???
+  }
+
+
+  // since optional values could be absent, trying to obtain the contained value
+  // is an *unsafe* operation UNLESS you provide a default in case the option is None
+  //
+  // Excercise: Using Option.getOrElse obtain the value contained in `rodeo` or if it is absent "its my first rodeo"
+  def fury(rodeo: Option[String]): String = ???
+
+}
--- rodeo/test/src/rodeo/OptionsSpec.scala
+++ rodeo/test/src/rodeo/OptionsSpec.scala
@@ -1,11 +1,23 @@
 package rodeo
 
-import zio.test.{ZIOSpecDefault, assertTrue}
+import zio.test._
+import zio.test.TestAspect.ignore
 
 object OptionsSpec extends ZIOSpecDefault {
+  import Options._
   def spec = suite("OptionsSpec")(
-    test("math works") {
-      assertTrue(2 + 2 == 4)
-    }
+    test("the answer to the universe exists") {
+      assertTrue(theAnwserToTheUniverseAndEverything.contains(42))
+    } @@ ignore,
+
+    test("matching on some") {
+      assertTrue(youKnow == "you know it all")
+    } @@ ignore,
+
+    test("getOrElse") {
+      assertTrue(fury(None) == "its my first rodeo") &&
+        assertTrue(fury(Some("my second rodeo")) == "my second rodeo")
+    } @@ ignore,
+
   )
 }
