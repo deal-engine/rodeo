08a3cef1455898512dfe5e5bf9cdc5b376ac5f09
Iván Molina Rebolledo <ivanmolinarebolledo@gmail.com>
2022-12-21 22:55:50 -0600

    Added a simple utility menu; added a basic preconf editor.

--- flake.nix
+++ flake.nix
@@ -1,15 +1,49 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-root.url = "github:srid/flake-root";
+    mission-control.url = "github:Platonic-Systems/mission-control";
   };
 
-  outputs = (inputs:
-  let
-    darwinPkgs = inputs.nixpkgs.legacyPackages.aarch64-darwin;
-  in
-  {
-    devShells.aarch64-darwin.default = with darwinPkgs; mkShell {
-      buildInputs = [ mill ];
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [
+        inputs.flake-root.flakeModule
+        inputs.mission-control.flakeModule
+      ];
+      perSystem = { pkgs, lib, config, system, ... }: {
+        mission-control.scripts = {
+          tests = {
+            description = "Run the Rodeo tests.";
+            exec = "mill -w rodeo.test";
+          };
+          editor = {
+            description = "Run a preconfigured VSCode.";
+            exec = "code .";
+          };
+        };
+        devShells.default =
+          let unfreepkgs = import nixpkgs { 
+            config = { allowUnfree = true; }; 
+            system = system;
+          };
+          vscode = unfreepkgs.vscode-with-extensions.override {
+            vscodeExtensions = with unfreepkgs.vscode-extensions; [
+              scalameta.metals
+              arrterian.nix-env-selector
+              scala-lang.scala
+              mkhl.direnv
+            ];
+          };
+          shell = pkgs.mkShell {
+            buildInputs = [ 
+              pkgs.mill
+              vscode
+            ];
+          };
+          in config.mission-control.installToDevShell shell;
+      };
     };
-  });
 }
