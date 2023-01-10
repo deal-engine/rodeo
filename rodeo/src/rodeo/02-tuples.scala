299092286280da2fffb53d5f592245bfc2e572d0
Iván Molina Rebolledo <ivanmolinarebolledo@gmail.com>
2023-01-10 04:47:06 -0600

    Simplifying Tuples.

--- rodeo/src/rodeo/02-tuples.scala
+++ rodeo/src/rodeo/02-tuples.scala
@@ -10,19 +10,15 @@ object Tuples extends Chapter {
   // Each element of a tuple can be of different type, for example, the
   // tuple containing an String and an Int is of type: Tuple2[String, Int]
   //
-  // In Scala2, tuples can hold from one to 22 different values,
-  // and their types are:  Tuple1[A], Tuple2[A, B], Tuple3[A, B, C], ...
+  // In Scala2, tuples can hold from one to 22 different values.
   //
   // In Scala2, there is syntactic sugar to shorten those types:
-  //   - Tuple1[A] = (A)
-  //   - Tuple2[A, B] = (A, B)
-  //   - Tuple3[A, B, C] = (A, B, C)
+  //   - Tuple1[Int] = (Int)
+  //   - Tuple2[Int, Boolean] = (Int, Boolean)
+  //   - Tuple3[Int, Boolean, Double] = (Int, Boolean, Double)
   //   - ...
   //
   // In Scala3 tuples are not restricted to 22 fields.
-  //
-  // In literature tuples are known as "products" `A x B` because they
-  // can hold for every possible value of A, every possible value of B.
 
   Exercise("Create a tuple containing your name and age") {
     val nameAndAge: (String, Int) = (???, ???)
